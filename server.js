ef99cbcd6ebf120d06ace127e197c07a193865e2
Josiah <josiahkj123@gmail.com>
2020-05-02 21:54:15 -0500

    added process.env to the port

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const URL = process.env.MONGODB_URI || "mongodb://localhost/";
 
 const app = express();
@@ -24,9 +23,6 @@ mongoose.connect(URL + "heroku_jx2x0c6q", {
 
 // routes
 app.use(require("./routes/api.js"));
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-})
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
